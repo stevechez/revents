f4c4d191d3569dcd35365faa8a57052ba700074b
Steve M <steve.maciaszek@sap.com>
2018-08-21 16:00:31 -0700

    new commits

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 // import HBar from './components/HorizBar/hbar'
-import { Button } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react'
+import EventDashboard from '../features/events/EventDashboard/EventDashboard'
+import NavBar from '../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div>
       <h1>re-vents</h1>
-        {/* <HBar /> */}
-      <Button icon="smile" content="React Button" />
+        
+      <NavBar />
+
+      <EventDashboard />
        
       </div>
     );
