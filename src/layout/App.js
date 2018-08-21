94436c948adbb3b49df4dee1227ce524f5cdefb9
Steve M <steve.maciaszek@sap.com>
2018-08-21 12:21:43 -0700

    initial commit

---
+++ src/layout/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+// import HBar from './components/HorizBar/hbar'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>re-vents</h1>
+        {/* <HBar /> */}
+
+       
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
