b4395fc436286eb64a3b898c3d375b3b1ff0678a
Steve M <stevem@Steves-MacBook-Pro.local>
2018-08-21 21:15:57 -0700

    changes

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-// import HBar from './components/HorizBar/hbar'
-import { Button } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import EventDashboard from '../features/events/EventDashboard/EventDashboard'
 import NavBar from '../features/nav/NavBar/NavBar'
 
@@ -8,12 +7,10 @@ class App extends Component {
   render() {
     return (
       <div>
-      <h1>re-vents</h1>
-        
       <NavBar />
-
-      <EventDashboard />
-       
+      <Container className="main">
+        <EventDashboard />
+      </Container>
       </div>
     );
   }
