07f8a62951c9e75adf00acfa7081ad4e08d2a8ba
stevechez <stevechez@gmail.com>
2018-09-10 16:15:33 -0700

    new commits

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
-import App from './layout/App';
+import './index.css';
+import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
+import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/scrollToTop';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
+}
+
+render();
+
 registerServiceWorker();
